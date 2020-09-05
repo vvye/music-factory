548041f85f784420e7c6abaeb3e234b583f8e664
vvye <ekaiser.hellwege@gmail.com>
2020-09-06 01:58:04 +0200

    Make cross junction piece randomize ball direction

--- js/CrossJunctionPiece.js
+++ js/CrossJunctionPiece.js
@@ -2,8 +2,28 @@ class CrossJunctionPiece extends Piece {
 
     static sprite;
 
+    processedBalls;
+
     constructor() {
         super();
+        this.processedBalls = [];
+    }
+
+    onBallInside(ball) {
+        if (this.processedBalls.includes(ball.id)) {
+            return;
+        }
+        if (this.ballNearCenter(ball)) {
+            this.setBallToCenter(ball);
+            random([ball.moveDown, ball.moveUp, ball.moveLeft, ball.moveRight]).bind(ball)();
+            this.processedBalls.push(ball.id);
+        }
+    }
+
+    onBallLeave(ball) {
+        if (this.processedBalls.includes(ball.id)) {
+            this.processedBalls.splice(this.processedBalls.indexOf(ball.id), 1);
+        }
     }
 
     draw() {
